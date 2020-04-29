d7ad33a3770053dfc121663eefb58cf225125c1a
Likhit <likhitgmella@gmail.com>
2020-04-29 23:30:38 +0530

    Alert-Pickers #1

--- VIT Share/ModifyRideViewController.swift
+++ VIT Share/ModifyRideViewController.swift
@@ -11,7 +11,21 @@ import Firebase
 import FirebaseDatabase
 import SkeletonView
 
-class ModifyRideViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+class ModifyRideViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let pickerData = ["VIT Vellore", "VIT Chennai", "Chennai Airport", "Bangalore Airport", "Tirupati Airport", "Katpadi Railway Station", "Chennai Railway Station", "Bangalore Railway Station", "Tirupati Railway Station", "Pondicherry", "Kodaikanal"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
     
     /* //////////////////////////////////////////////////////////////////////////////// */
     
@@ -49,6 +63,7 @@ class ModifyRideViewController: UIViewController, UICollectionViewDataSource, UI
         let ride = ridesList[indexPath.row]
         let alertController = UIAlertController(title: "Update", message: "Update Values", preferredStyle: .alert)
         
+        // Update action
         let updateAction = UIAlertAction(title: "Update", style: .default) { (_) in
             let id = ride.id
             let newFrom = alertController.textFields?[0].text
@@ -58,26 +73,75 @@ class ModifyRideViewController: UIViewController, UICollectionViewDataSource, UI
             self.updateRide(id: id!, newFrom: newFrom!, newTo: newTo!, newSeats: newSeats!, newDateTime: newDateTime!)
         }
         
+        // Adding Update action
+        alertController.addAction(updateAction)
+        
+        // Picker view
+        let pickerView = UIPickerView()
+        alertController.view.addSubview(pickerView) // Adding picker view to the alert controller view
+        pickerView.delegate = self                  // Set delegate
+        pickerView.dataSource = self                // Set datasource
+        
+        // Date and Time picker
+        let datePicker = UIDatePicker()
+        
+        // Toolbar, upon Picker
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        // Bar button, in Toolbar
+        let doneBtn = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: nil)
+        toolbar.setItems([doneBtn], animated: true)
+        
+        // #1
+        alertController.addTextField{(textField) in
+            textField.text = ride.from
+            textField.inputView = pickerView
+            
+            // Assign toolbar to the keyboard
+            textField.inputAccessoryView = toolbar
+        }
+        
+        // #2
+        alertController.addTextField{(textField) in
+            textField.text = ride.to
+            textField.inputView = pickerView
+            
+            // Assign toolbar to the keyboard
+            textField.inputAccessoryView = toolbar
+        }
+        
+        // #3
+        alertController.addTextField{(textField) in
+            textField.text = ride.seats
+            textField.keyboardType = .decimalPad
+        }
+        
+        // #4
+        alertController.addTextField{(textField) in
+            textField.text = ride.dateTime
+            textField.inputView = datePicker
+            
+            // Assign toolbar to the keyboard
+            textField.inputAccessoryView = toolbar
+            
+            // Assign date picker to the textfield
+            textField.inputView = datePicker
+            
+            // Date picker mode
+            datePicker.datePickerMode = .dateAndTime
+        }
+        
+        // Delete action
         let deleteAction = UIAlertAction(title: "Delete", style: .default) { (_) in
             
         }
-
-        alertController.addAction(updateAction)
-        alertController.addAction(deleteAction)
-        present(alertController, animated: true, completion: nil)
         
-        alertController.addTextField{(textField) in
-            textField.text = ride.from
-        }
-        alertController.addTextField{(textField) in
-            textField.text = ride.to
-        }
-        alertController.addTextField{(textField) in
-            textField.text = ride.seats
-        }
-        alertController.addTextField{(textField) in
-            textField.text = ride.dateTime
-        }
+        // Adding Update action
+        alertController.addAction(deleteAction)
+        
+        // Present Alert controller
+        present(alertController, animated: true, completion: nil)
         
     }
     
@@ -134,4 +198,4 @@ class ModifyRideViewController: UIViewController, UICollectionViewDataSource, UI
         
     }
     
-}   // #138
+}   // #202
