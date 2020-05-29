09a2e02941eeefc9c14ab6c4cffe1ce22dcedec0
Likhit <likhitgmella@gmail.com>
2020-05-29 14:34:05 +0530

    cell-4 create

---
+++ VIT Share/Cell4.swift
@@ -0,0 +1,18 @@
+//
+//  Cell4.swift
+//  VIT Share
+//
+//  Created by Likhit Garimella on 29/05/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class Cell4: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
