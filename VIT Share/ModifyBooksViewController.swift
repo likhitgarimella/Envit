26b61fb2e84a6187df739886e2690823c32db682
Likhit <likhitgmella@gmail.com>
2020-05-29 14:57:56 +0530

    register cell-4 in modify books

--- VIT Share/ModifyBooksViewController.swift
+++ VIT Share/ModifyBooksViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ModifyBooksViewController: UIViewController {
+class ModifyBooksViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    // Collection View
+    @IBOutlet var cardCollectionView2: UICollectionView!
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return 10
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // Registering cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell4", for: indexPath) as! Cell4
+        
+        return cell
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        // Register CollectionViewCell 'Cell4' here
+        cardCollectionView2.register(UINib.init(nibName: "Cell4", bundle: nil), forCellWithReuseIdentifier: "Cell4")
+        if let flowLayout = cardCollectionView2.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.estimatedItemSize = CGSize(width: 1, height: 1)
+        }
         
     }
 
-}   // #21
+}   // #43
