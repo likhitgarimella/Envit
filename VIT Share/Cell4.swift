e1ddbc09ea42ea2efa3e50f5a8b07311f0dd7b8d
Likhit <likhitgmella@gmail.com>
2020-05-29 14:48:52 +0530

    cell-4 xib modified

--- VIT Share/Cell4.swift
+++ VIT Share/Cell4.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class Cell4: UICollectionViewCell {
 
+    // Outlets
+    @IBOutlet var cardView2: UIView!
+    @IBOutlet var widthConstraint2: NSLayoutConstraint!
+    @IBOutlet var bookTitle2: UILabel!
+    @IBOutlet var bookDescription2: UILabel!
+    @IBOutlet var conditionLabel2: UILabel!
+    @IBOutlet var conditionValue2: UILabel!
+    @IBOutlet var price2: UILabel!
+    
+    @IBOutlet var timeAgo2: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        cardView2.layer.cornerRadius = 10
+        self.contentView.translatesAutoresizingMaskIntoConstraints = false
+        let screenWidth = UIScreen.main.bounds.size.width
+        widthConstraint2.constant = screenWidth - (2 * 12)
+        
     }
 
-}
+}   // #35
