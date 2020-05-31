6fcbd1fc2b702f3f7de680c9e8301250e5521635
Likhit <likhitgmella@gmail.com>
2020-05-31 14:39:26 +0530

    app new setup

---
+++ VIT Share/ProjectsShareViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectsShareViewController.swift
+//  VIT Share
+//
+//  Created by Likhit Garimella on 31/05/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class ProjectsShareViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
---
+++ VIT Share/TechShareViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TechShareViewController.swift
+//  VIT Share
+//
+//  Created by Likhit Garimella on 31/05/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class TechShareViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+}   // #21
