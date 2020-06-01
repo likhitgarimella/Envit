5f8f31495d4cdc6e0b5e153607357c6c6cc87574
Likhit <likhitgmella@gmail.com>
2020-06-01 13:30:07 +0530

    vc setup #1

---
+++ VIT Share/FindMentorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FindMentorViewController.swift
+//  VIT Share
+//
+//  Created by Likhit Garimella on 01/06/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class FindMentorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+}   // #21
---
+++ VIT Share/ModifyTechViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ModifyTechViewController.swift
+//  VIT Share
+//
+//  Created by Likhit Garimella on 01/06/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class ModifyTechViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
