f195ea58934c6b08099e47b3949bf12ffd06a1b3
Likhit <likhitgmella@gmail.com>
2020-06-01 23:22:41 +0530

    mentor vc #2

--- VIT Share/MentorViewController.swift
+++ VIT Share/MentorViewController.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class MentorViewController: UIViewController {
+class MentorViewController: UIViewController, UITextViewDelegate {
     
     // Outlets
     @IBOutlet var domain: DropDown!
+    @IBOutlet var experienceTextView: UITextView!
+    @IBOutlet var prerequisites: UITextField!
+    @IBOutlet var courses: UITextField!
+    @IBOutlet var submitOutlet: UIButton!
     
     func BorderProp() {
         
@@ -19,6 +23,12 @@ class MentorViewController: UIViewController {
         let myColor = UIColor.systemGray
         domain.layer.borderColor = myColor.cgColor
         domain.layer.borderWidth = 1.6
+        experienceTextView.layer.borderColor = myColor.cgColor
+        experienceTextView.layer.borderWidth = 1.6
+        prerequisites.layer.borderColor = myColor.cgColor
+        prerequisites.layer.borderWidth = 1.6
+        courses.layer.borderColor = myColor.cgColor
+        courses.layer.borderWidth = 1.6
         
     }
     
@@ -26,6 +36,10 @@ class MentorViewController: UIViewController {
         
         // Textfield Corner Radius Property
         domain.layer.cornerRadius = 4
+        experienceTextView.layer.cornerRadius = 4
+        prerequisites.layer.cornerRadius = 4
+        courses.layer.cornerRadius = 4
+        submitOutlet.layer.cornerRadius = 6
         
     }
     
@@ -34,6 +48,10 @@ class MentorViewController: UIViewController {
         // Create a padding view for Credits TextFields on LEFT
         domain.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: domain.frame.height))
         domain.leftViewMode = .always
+        prerequisites.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: prerequisites.frame.height))
+        prerequisites.leftViewMode = .always
+        courses.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: courses.frame.height))
+        courses.leftViewMode = .always
         
     }
     
@@ -44,16 +62,26 @@ class MentorViewController: UIViewController {
         domain.selectedRowColor = UIColor(red: 220.0/255.0, green: 220.0/255.0, blue: 220.0/255.0, alpha: 1.0)
         
     }
-
+    
+    func Properties() {
+        
+        experienceTextView.backgroundColor = UIColor.white
+        // padding for text view
+        experienceTextView.textContainerInset = UIEdgeInsets(top: 6, left: 6, bottom: 6, right: 8)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         hideKeyboardWhenTappedAround()
+        
         BorderProp()
         CornerRadius()
         LeftPadding()
         DropDownOptions()
+        Properties()
         
     }
 
-}   // #60
+}   // #88
