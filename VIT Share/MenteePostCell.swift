a13e625ec892a29c69348ae009b5133813abcd8a
Likhit <likhitgmella@gmail.com>
2020-06-02 16:28:37 +0530

    cell xibs

---
+++ VIT Share/MenteePostCell.swift
@@ -0,0 +1,20 @@
+//
+//  MenteePostCell.swift
+//  VIT Share
+//
+//  Created by Likhit Garimella on 02/06/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class MenteePostCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+
+}   // #21
---
+++ VIT Share/MentorPostCell.swift
@@ -0,0 +1,20 @@
+//
+//  MentorPostCell.swift
+//  VIT Share
+//
+//  Created by Likhit Garimella on 02/06/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class MentorPostCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+
+}   // #21
