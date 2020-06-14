90f95fc1406636b85685f2ef3cde17ddc0f1df7b
Likhit <likhitgmella@gmail.com>
2020-06-15 05:02:40 +0530

    mentor feed vc #1

--- VIT Share/FeedViewController.swift
+++ VIT Share/FeedViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 
 class FeedViewController: UIViewController {
     
-    // Outlets
+    // Switch Outlets
     @IBOutlet var mentorSwitch: UISwitch!
     @IBOutlet var menteeSwitch: UISwitch!
     
+    // Container Views
     @IBOutlet var mentorFeedView: UIView!
     @IBOutlet var menteeFeedView: UIView!
     
@@ -25,6 +26,7 @@ class FeedViewController: UIViewController {
         
     }
     
+    // 1st switch action
     @IBAction func enableMentor(_ sender: UISwitch) {
         
         menteeSwitch.setOn(!sender.isOn, animated: true)
@@ -41,6 +43,7 @@ class FeedViewController: UIViewController {
         
     }
     
+    // 2nd switch action
     @IBAction func enableMentee(_ sender: UISwitch) {
         
         mentorSwitch.setOn(!sender.isOn, animated: true)
@@ -56,37 +59,6 @@ class FeedViewController: UIViewController {
         }
         
     }
-    
-    var mentorList = [MentorModel]()
-    
-    var refMentors: DatabaseReference!
-    
-    
-     /*
-     
-        else {
-            let mentorCell = feedCollectionView.dequeueReusableCell(withReuseIdentifier: "MentorPostCell", for: indexPath) as! MentorPostCell
-            // cell.hideAnimation()
-            let mentor: MentorModel
-            mentor = mentorList[indexPath.row]
-            mentorCell.domainName.text = mentor.domainText
-            mentorCell.experienceTextView.text = mentor.experienceText
-            mentorCell.courseTextView.text = mentor.coursesText
-            mentorCell.prerequisiteTextView.text = mentor.prerequisitesText
-            /* if let seconds = mentor.timestamp {
-                // let timeStampDate = NSDate(timeIntervalSince1970: seconds)
-                let pastDate = Date(timeIntervalSince1970: seconds)
-                // let dateFormatter = DateFormatter()
-                // dateFormatter.dateFormat = "MMM d, h:mm a"
-                // cell.timeAgo.text = dateFormatter.string(from: timeStampDate as Date)
-                // cell.timeAgo.text = pastDate.timeAgoDisplay()
-            } */
-            return mentorCell
-        }
-        
-    }
- 
-     */
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -94,43 +66,6 @@ class FeedViewController: UIViewController {
         Switch()
         hideKeyboardWhenTappedAround()
         
-        /*
-        
-        // Register CollectionViewCell 'MentorPostCell' here
-        feedCollectionView.register(UINib.init(nibName: "MentorPostCell", bundle: nil), forCellWithReuseIdentifier: "MentorPostCell")
-        if let flowLayout = feedCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
-            flowLayout.estimatedItemSize = CGSize(width: 1, height: 1)
-        }
-        
-        //
-        refMentors = Database.database().reference().child("Mentors").child("Details")
-        refMentors.observe(DataEventType.value, with: { (snapshot) in
-            
-            if snapshot.childrenCount > 0 {
-                
-                self.mentorList.removeAll()
-                for mentors in snapshot.children.allObjects as! [DataSnapshot] {
-                    let mentorObject = mentors.value as? [String: AnyObject]
-                    let mentorId = mentorObject?["1) id"]
-                    let mentorDomain  = mentorObject?["2) Domain"]
-                    let mentorExp  = mentorObject?["3) Experience"]
-                    let mentorPrereq = mentorObject?["4) Prerequisites"]
-                    let mentorCourses = mentorObject?["5) Courses"]
-                    let mentorTimestamp = mentorObject?["6) Timestamp"]
-                    
-                    let mentor = MentorModel(id: mentorId as! String?, domainText: mentorDomain as! String?, experienceText: mentorExp as! String?, prerequisitesText: mentorPrereq as! String?, coursesText: mentorCourses as! String?, timestamp: mentorTimestamp as! Double?)
-                    self.mentorList.append(mentor)
-                }
-                self.feedCollectionView.reloadData()
-                
-            }
-            
-        })
-        
-        //
- 
-        */
-        
     }
     
-}   // #201
+}   // #72
--- VIT Share/MentorFeedViewController.swift
+++ VIT Share/MentorFeedViewController.swift
@@ -7,14 +7,77 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
 
-class MentorFeedViewController: UIViewController {
+class MentorFeedViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    @IBOutlet var mentorFeedCollectionView: UICollectionView!
+    
+    // numberOfItemsInSection
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return mentorList.count
+    }
+    
+    // cellForItemAt
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let mentorCell = mentorFeedCollectionView.dequeueReusableCell(withReuseIdentifier: "MentorPostCell", for: indexPath) as! MentorPostCell
+        
+        let mentor: MentorModel
+        mentor = mentorList[indexPath.row]
+        mentorCell.domainName.text = mentor.domainText
+        mentorCell.experienceTextView.text = mentor.experienceText
+        mentorCell.courseTextView.text = mentor.coursesText
+        mentorCell.prerequisiteTextView.text = mentor.prerequisitesText
+        /* if let seconds = mentor.timestamp {
+            // let timeStampDate = NSDate(timeIntervalSince1970: seconds)
+            let pastDate = Date(timeIntervalSince1970: seconds)
+            // let dateFormatter = DateFormatter()
+            // dateFormatter.dateFormat = "MMM d, h:mm a"
+            // cell.timeAgo.text = dateFormatter.string(from: timeStampDate as Date)
+            // cell.timeAgo.text = pastDate.timeAgoDisplay()
+        } */
+        return mentorCell
+        
+    }
+    
+    var mentorList = [MentorModel]()
+    var refMentors: DatabaseReference!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Register CollectionViewCell 'MentorPostCell' here
+        mentorFeedCollectionView.register(UINib.init(nibName: "MentorPostCell", bundle: nil), forCellWithReuseIdentifier: "MentorPostCell")
+        if let flowLayout = mentorFeedCollectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            flowLayout.estimatedItemSize = CGSize(width: 1, height: 1)
+        }
         
+        refMentors = Database.database().reference().child("Mentors").child("Details")
+        refMentors.observe(DataEventType.value, with: { (snapshot) in
+            
+            if snapshot.childrenCount > 0 {
+                
+                self.mentorList.removeAll()
+                for mentors in snapshot.children.allObjects as! [DataSnapshot] {
+                    let mentorObject = mentors.value as? [String: AnyObject]
+                    let mentorId = mentorObject?["1) id"]
+                    let mentorDomain  = mentorObject?["2) Domain"]
+                    let mentorExp  = mentorObject?["3) Experience"]
+                    let mentorPrereq = mentorObject?["4) Prerequisites"]
+                    let mentorCourses = mentorObject?["5) Courses"]
+                    let mentorTimestamp = mentorObject?["6) Timestamp"]
+                    
+                    let mentor = MentorModel(id: mentorId as! String?, domainText: mentorDomain as! String?, experienceText: mentorExp as! String?, prerequisitesText: mentorPrereq as! String?, coursesText: mentorCourses as! String?, timestamp: mentorTimestamp as! Double?)
+                    self.mentorList.append(mentor)
+                }
+                self.mentorFeedCollectionView.reloadData()
+                
+            }
+            
+        })
         
     }
     
-}   // #21
+}   // #84
