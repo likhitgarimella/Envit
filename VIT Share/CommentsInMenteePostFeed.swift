cd45d34468e394c775eb1f469f04c5da985e93d5
Likhit <likhitgmella@gmail.com>
2020-06-16 04:35:36 +0530

    ~comments in mentee post~ vc #2

--- VIT Share/CommentsInMenteePostFeed.swift
+++ VIT Share/CommentsInMenteePostFeed.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class CommentsInMenteePostFeed: UIViewController {
     
@@ -17,6 +18,18 @@ class CommentsInMenteePostFeed: UIViewController {
     
     @IBOutlet var bottomConstraint: NSLayoutConstraint!
     
+    // Tab bar disappears
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    // Tab bar appears
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        self.tabBarController?.tabBar.isHidden = false
+    }
+    
     func Properties() {
         
         commentsInMenteePostFeedTableView.backgroundColor = UIColor.white
@@ -24,11 +37,58 @@ class CommentsInMenteePostFeed: UIViewController {
         commentsInMenteePostFeedTableView.rowHeight = UITableView.automaticDimension
         
     }
+    
+    func handleTextField() {
+        
+        commentTextField.addTarget(self, action: #selector(textFieldDidChange), for: .editingChanged)
+        
+    }
+    
+    @objc func textFieldDidChange() {
+        
+        if let commentText = commentTextField.text, !commentText.isEmpty {
+            sendOutlet.setTitleColor(UIColor.systemBlue, for: .normal)
+            sendOutlet.isEnabled = true
+            return
+        }
+        
+        sendOutlet.setTitleColor(UIColor.lightGray, for: .normal)
+        sendOutlet.isEnabled = false
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        hideKeyboardWhenTappedAround()
         Properties()
+        handleTextField()
+        
+        // Keyboard Show
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        // Keyboard Hide
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+    }
+    
+    // Keyboard Show
+    @objc func keyboardWillShow(notification: NSNotification) {
+        
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+        
+    }
+    
+    // Keyboard Hide
+    @objc func keyboardWillHide(notification: NSNotification) {
+        
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
         
     }
     
@@ -37,7 +97,7 @@ class CommentsInMenteePostFeed: UIViewController {
 extension CommentsInMenteePostFeed: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -46,4 +106,4 @@ extension CommentsInMenteePostFeed: UITableViewDataSource {
         return cell
     }
     
-}   // #50
+}   // #110
