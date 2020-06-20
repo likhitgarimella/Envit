12bbb93107fdfbf2bc8d2e338952463518ab909d
Likhit <likhitgmella@gmail.com>
2020-06-20 19:41:39 +0530

    api.swift

--- VIT Share/Api.swift
+++ VIT Share/Api.swift
@@ -1,6 +1,6 @@
 //
 //  Api.swift
-//  Swiftagram
+//  VIT Share
 //
 //  Created by Likhit Garimella on 18/06/20.
 //  Copyright © 2020 Likhit Garimella. All rights reserved.
@@ -12,9 +12,13 @@ import Foundation
 
 struct Api {
     
+    /// Firebase node - Users
     static var User = UserApi()
+    /// Firebase node - Mentees-Details
     static var MenteePost = MenteePostApi()
+    /// Firebase node - Comments-In-Mentee-Post
     static var MenteeComment = MenteeCommentApi()
+    /// Firebase node - Mentee-Post-Comments
     static var MenteePostComment = MenteePostCommentApi()
     
-}   // #21
+}   // #25
