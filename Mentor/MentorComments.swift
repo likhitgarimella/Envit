607fc32fd30c03ff75ac19b8abb4fc5921fe1108
Likhit <likhitgmella@gmail.com>
2020-06-21 06:22:01 +0530

    MentorComments.swift

--- Mentor/MentorComments.swift
+++ Mentor/MentorComments.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+class MentorComments {
+    
+    var commentText: String?
+    var uid: String?
+    
+}
+
+extension MentorComments {
+    
+    static func transformComment(dict: [String: Any]) -> MentorComments {
+        
+        let comment = MentorComments()
+        comment.commentText = dict["commentText"] as? String
+        comment.uid = dict["uid"] as? String
+        return comment
+        
+    }
+    
+}   // #30
