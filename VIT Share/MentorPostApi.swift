92284801a7b2732d15334854e1e152d51046b6fd
Likhit <likhitgmella@gmail.com>
2020-06-21 06:10:32 +0530

    MentorPostApi.swift

--- VIT Share/MentorPostApi.swift
+++ VIT Share/MentorPostApi.swift
@@ -7,3 +7,25 @@
 //
 
 import Foundation
+import Firebase
+
+/// Write your own Api, to conveniently observe database data...
+
+class MentorPostApi {
+    
+    var REF_POSTS = Database.database().reference().child("Mentors").child("Details")
+    
+    func observePosts(completion: @escaping (MentorModel) -> Void) {
+        
+        REF_POSTS.observe(.childAdded, with: { (snapshot) in
+            
+            if let dict = snapshot.value as? [String: Any] {
+                let newPost = MentorModel.transformMentorPost(dict: dict, key: snapshot.key)
+                completion(newPost)
+            }
+            
+        })
+        
+    }
+    
+}   // #32
