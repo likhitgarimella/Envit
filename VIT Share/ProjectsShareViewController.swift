05c0b2786393fc9dd3402bf3e7461059c8372c27
Likhit <likhitgmella@gmail.com>
2020-06-25 05:01:33 +0530

    ~PS~ screen-1 #3

--- VIT Share/ProjectsShareViewController.swift
+++ VIT Share/ProjectsShareViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ProjectsShareViewController: UIViewController {
+    
+    // Outlets
+    @IBOutlet var addProjectOutlet: UIButton!
+    
+    func AddProjectBtnProp() {
+        
+        addProjectOutlet.layer.cornerRadius = 10
+        addProjectOutlet.layer.borderWidth = 2.5
+        addProjectOutlet.layer.borderColor = UIColor(red: 255/255, green: 119/255, blue: 119/255, alpha: 1.0).cgColor
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        AddProjectBtnProp()
         
     }
     
-}   // #21
+}   // #32
