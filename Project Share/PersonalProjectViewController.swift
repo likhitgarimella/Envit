d388267a9c151e7d65cccfaa021a8b7810afc6d8
Likhit <likhitgmella@gmail.com>
2020-06-26 05:24:30 +0530

    ~PS~ pp #2

--- Project Share/PersonalProjectViewController.swift
+++ Project Share/PersonalProjectViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class PersonalProjectViewController: UIViewController {
+    
+    // Outlets
+    @IBOutlet var projectTitle: UITextField!
+    @IBOutlet var roleOption: DropDown!
+    
+    func BorderProp() {
+        
+        // Textfield Border Property
+        let myColor = UIColor(red: 63/255, green: 85/255, blue: 132/255, alpha: 1.0)
+        projectTitle.layer.borderColor = myColor.cgColor
+        projectTitle.layer.borderWidth = 1.6
+        roleOption.layer.borderColor = myColor.cgColor
+        roleOption.layer.borderWidth = 1.6
+        
+    }
+    
+    func CornerRadius() {
+        
+        // Textfield Corner Radius Property
+        projectTitle.layer.cornerRadius = 4
+        roleOption.layer.cornerRadius = 22
 
+        
+    }
+    
+    func LeftPadding() {
+        
+        // Create a padding view for Textfields on LEFT
+        projectTitle.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: projectTitle.frame.height))
+        projectTitle.leftViewMode = .always
+        roleOption.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: roleOption.frame.height))
+        roleOption.leftViewMode = .always
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         hideKeyboardWhenTappedAround()
+        BorderProp()
+        CornerRadius()
+        LeftPadding()
         
     }
     
-}   // #21
+}   // #58
