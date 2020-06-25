d53eff31305388c38efa4edfeeae153ecb81a79c
Likhit <likhitgmella@gmail.com>
2020-06-26 05:02:04 +0530

    ~PS~ pp #1

---
+++ Project Share/JComponentViewController.swift
@@ -0,0 +1,20 @@
+//
+//  JComponentViewController.swift
+//  VIT Share
+//
+//  Created by Likhit Garimella on 26/06/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class JComponentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
---
+++ Project Share/PersonalProjectViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PersonalProjectViewController.swift
+//  VIT Share
+//
+//  Created by Likhit Garimella on 26/06/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class PersonalProjectViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        hideKeyboardWhenTappedAround()
+        
+    }
+    
+}   // #21
