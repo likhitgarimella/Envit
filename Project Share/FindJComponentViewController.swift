76621decca1679d25ea4e22de57ccad045a19c1c
Likhit <likhitgmella@gmail.com>
2020-06-26 19:15:23 +0530

    ~PS~ 2 VCs setup

---
+++ Project Share/FindJComponentViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FindJComponentViewController.swift
+//  VIT Share
+//
+//  Created by Likhit Garimella on 26/06/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class FindJComponentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
---
+++ Project Share/FindNewProjectsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FindNewProjectsViewController.swift
+//  VIT Share
+//
+//  Created by Likhit Garimella on 26/06/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class FindNewProjectsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
