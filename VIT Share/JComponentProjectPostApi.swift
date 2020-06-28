ac009ae63360876eed705892a7c4be1161c218af
Likhit <likhitgmella@gmail.com>
2020-06-29 01:47:09 +0530

    jcomp proj post api

--- VIT Share/JComponentProjectPostApi.swift
+++ VIT Share/JComponentProjectPostApi.swift
@@ -7,9 +7,25 @@
 //
 
 import Foundation
+import Firebase
+
+/// Write your own Api, to conveniently observe database data...
 
 class JComponentProjectPostApi {
     
+    var REF_JCOMP_PROJ_POSTS = Database.database().reference().child("J-Component Projects").child("Details")
     
+    func observePosts(completion: @escaping (JComponentProjectModel) -> Void) {
+        
+        REF_JCOMP_PROJ_POSTS.observe(.childAdded, with: { (snapshot) in
+            
+            if let dict = snapshot.value as? [String: Any] {
+                let newPost = JComponentProjectModel.transformJCompProjPost(dict: dict, key: snapshot.key)
+                completion(newPost)
+            }
+            
+        })
+        
+    }
     
-}   // #16
+}   // #32
