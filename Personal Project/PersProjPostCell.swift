e21a554ae22c680400418a440e2178eb568476e6
Likhit <likhitgmella@gmail.com>
2020-06-28 12:01:10 +0530

    pers proj post cell #1

--- Personal Project/PersProjPostCell.swift
+++ Personal Project/PersProjPostCell.swift
@@ -11,6 +11,21 @@ import Firebase
 
 class PersProjPostCell: UICollectionViewCell {
     
+    // Outlets
+    @IBOutlet var cardView: UIView!
+    @IBOutlet var widthConstraint: NSLayoutConstraint!
+    
+    @IBOutlet var personName: UILabel!
+    @IBOutlet var roleName: UILabel!
+    @IBOutlet var projectName: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    @IBOutlet var bottomView: UIView!
+    @IBOutlet var mentorLikeImageView: UIImageView!
+    @IBOutlet var mentorCommentImageView: UIImageView!
+    
+    @IBOutlet var likeCountButton: UIButton!
+    
     // linking pers proj feed VC & pers proj post cell
     var persProjFeedVC: FindNewProjectsViewController?
     
@@ -27,15 +42,48 @@ class PersProjPostCell: UICollectionViewCell {
     /// we'll let the cell download the correspoding cell..
     var user: User? {
         didSet {
-            
+            setupUserInfo()
         }
+    }
+    
+    /// New setupUserInfo() func
+    func setupUserInfo() {
+        
+        personName.text = user?.nameString
+        
     }
 
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        // initial text
+        personName.text = ""
+        roleName.text = ""
+        projectName.text = ""
+        descriptionLabel.text = ""
         
+        // corner radius
+        cardView.layer.cornerRadius = 10
+        bottomView.layer.cornerRadius = 10
+        
+        // shadow properties
+        cardView.layer.shadowColor = UIColor.lightGray.cgColor
+        cardView.layer.shadowOffset = CGSize(width: 2.0, height: 2.0)
+        cardView.layer.shadowRadius = 2.0
+        cardView.layer.shadowOpacity = 0.5
+        cardView.layer.masksToBounds = false
+        
+        bottomView.layer.shadowColor = UIColor.lightGray.cgColor
+        bottomView.layer.shadowOffset = CGSize(width: 2.0, height: 2.0)
+        bottomView.layer.shadowRadius = 2.0
+        bottomView.layer.shadowOpacity = 0.5
+        bottomView.layer.masksToBounds = false
+        
+        // constraint
+        self.contentView.translatesAutoresizingMaskIntoConstraints = false
+        let screenWidth = UIScreen.main.bounds.size.width
+        widthConstraint.constant = screenWidth - (2 * 12)
         
     }
 
-}   // #42
+}   // #90
