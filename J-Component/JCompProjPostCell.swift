8408e880e9e5da91b376762509ab5646fd15a308
Likhit <likhitgmella@gmail.com>
2020-06-29 00:21:24 +0530

    jcomp proj post cell files added

---
+++ J-Component/JCompProjPostCell.swift
@@ -0,0 +1,20 @@
+//
+//  JCompProjPostCell.swift
+//  VIT Share
+//
+//  Created by Likhit Garimella on 29/06/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class JCompProjPostCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+        
+    }
+
+}   // #21
