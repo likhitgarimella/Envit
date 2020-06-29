83f11d5c514748ca120eef461c1e2ff674f18375
Likhit <likhitgmella@gmail.com>
2020-06-29 11:29:50 +0530

    3 new files added for jcomp comments

---
+++ J-Component/CommentsInJCompProjFeed.swift
@@ -0,0 +1,20 @@
+//
+//  CommentsInJCompProjFeed.swift
+//  VIT Share
+//
+//  Created by Likhit Garimella on 29/06/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class CommentsInJCompProjFeed: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
---
+++ J-Component/JCompProjectComments.swift
@@ -0,0 +1,9 @@
+//
+//  JCompProjectComments.swift
+//  VIT Share
+//
+//  Created by Likhit Garimella on 29/06/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import Foundation
