9cbadebb5431d7c6668d97d3513c388ffe4b91a2
Likhit <likhitgmella@gmail.com>
2020-06-29 12:50:40 +0530

    jcomp proj post cell #2

--- J-Component/JCompProjPostCell.swift
+++ J-Component/JCompProjPostCell.swift
@@ -34,7 +34,7 @@ class JCompProjPostCell: UICollectionViewCell {
     
     var jCompProjPost: JComponentProjectModel? {
         didSet {
-            
+            updateView()
         }
     }
     
@@ -44,6 +44,66 @@ class JCompProjPostCell: UICollectionViewCell {
         didSet {
             setupUserInfo()
         }
+    }
+    
+    func updateView() {
+        
+        courseName.text = jCompProjPost?.courseTitle
+        projectName.text = jCompProjPost?.projectTitle
+        descriptionLabel.text = jCompProjPost?.projDesc
+        
+        setupUserInfo()
+        
+        /// Update like
+        updateLike(post: jCompProjPost!)
+        
+        /// Update like count
+        Api.JComponentProjectPost.REF_JCOMP_PROJ_POSTS.child(jCompProjPost!.id!).observe(.childChanged, with: {
+            snapshot in
+            print(snapshot)
+            if let value = snapshot.value as? Int {
+                self.likeCountButton.setTitle("\(value) likes", for: .normal)
+            }
+        })
+        
+        /// Smoothly update like, when scrolling view
+        Api.JComponentProjectPost.REF_JCOMP_PROJ_POSTS.child(jCompProjPost!.id!).observeSingleEvent(of: .value, with: {
+            snapshot in
+            if let dict = snapshot.value as? [String: Any] {
+                let post = JComponentProjectModel.transformJCompProjPost(dict: dict, key: snapshot.key)
+                self.updateLike(post: post)
+            }
+        })
+        
+    }
+    
+    func updateLike(post: JComponentProjectModel) {
+        
+        // print(post.isLiked)
+        /// we first checked if its true, and no one liked this post before..
+        /// or if probably someone did, but the current user did not..
+        /// then we display, non-selected like icon..
+        /// otherwise, display likeSelected icon..
+        let imageName = post.likes == nil || !post.isLiked! ? "Icon1" : "likeSelected"
+        projectLikeImageView.image = UIImage(named: imageName)
+        /// Below commented snippet can be put in 1 line.. as above..
+        /* if post.isLiked == false {
+            likeImageView.image = UIImage(named: "like")
+        } else {
+            likeImageView.image = UIImage(named: "likeSelected")
+        } */
+        
+        // We now update like count
+        /// Use optional chaining with guard
+        guard let count = post.likeCount else {
+            return
+        }
+        if count != 0 {
+            likeCountButton.setTitle("\(count) likes", for: .normal)
+        } else {
+            likeCountButton.setTitle("0 likes", for: .normal)
+        }
+        
     }
     
     /// New setupUserInfo() func
@@ -86,4 +146,4 @@ class JCompProjPostCell: UICollectionViewCell {
         
     }
 
-}   // #90
+}   // #150
