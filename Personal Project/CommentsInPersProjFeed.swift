1729efb15f980ec406b5e8b3b2545d7730c69ae0
Likhit <likhitgmella@gmail.com>
2020-06-29 10:27:17 +0530

    comments in pers proj vc #1

--- Personal Project/CommentsInPersProjFeed.swift
+++ Personal Project/CommentsInPersProjFeed.swift
@@ -7,8 +7,42 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
 class CommentsInPersProjFeed: UIViewController {
+    
+    @IBOutlet var commentsInPersProjFeedTableView: UITableView!
+    
+    @IBOutlet var commentTextField: UITextField!
+    @IBOutlet var sendOutlet: UIButton!
+    
+    // image on send button
+    @IBOutlet var sendImage: UIImageView!
+    
+    @IBOutlet var bottomConstraint: NSLayoutConstraint!
+    
+    // dummy post id taken for example
+    // let postId = "-M9o5UApAuGFRj8zHH14"
+    
+    // Initialise empty string
+    var postId: String!
+    
+    var mentorComments = [PersProjectComments]()
+    var users = [User]()
+    
+    // Tab bar disappears
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    // Tab bar appears
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        self.tabBarController?.tabBar.isHidden = false
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,4 +51,4 @@ class CommentsInPersProjFeed: UIViewController {
         
     }
     
-}   // #21
+}   // #55
