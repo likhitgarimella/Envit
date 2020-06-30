ea502acf519b2c8609f1d0b1bb1f65c1732a7b56
Likhit <likhitgmella@gmail.com>
2020-06-30 10:38:06 +0530

    comments in jcomp feed vc #1

--- J-Component/CommentsInJCompProjFeed.swift
+++ J-Component/CommentsInJCompProjFeed.swift
@@ -7,8 +7,42 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
 
 class CommentsInJCompProjFeed: UIViewController {
+    
+    @IBOutlet var commentsInJCompProjFeedTableView: UITableView!
+    
+    @IBOutlet var commentTextField: UITextField!
+    @IBOutlet var sendOutlet: UIButton!
+    
+    // image on send button
+    @IBOutlet var sendImage: UIImageView!
+    
+    @IBOutlet var bottomConstraint: NSLayoutConstraint!
+    
+    // dummy post id taken for example
+    // let postId = "-M9o5UApAuGFRj8zHH14"
+    
+    // Initialise empty string
+    var postId: String!
+    
+    var jCompProjComments = [JCompProjectComments]()
+    var users = [User]()
+    
+    // Tab bar disappears
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        self.tabBarController?.tabBar.isHidden = true
+    }
+    
+    // Tab bar appears
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        self.tabBarController?.tabBar.isHidden = false
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,4 +51,4 @@ class CommentsInJCompProjFeed: UIViewController {
         
     }
     
-}   // #21
+}   // #55
