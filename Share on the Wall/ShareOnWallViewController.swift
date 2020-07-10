cdd237dad8dae7af69a6922e0c5c8c70d3978b80
Likhit <likhitgmella@gmail.com>
2020-07-10 12:11:49 +0530

    share on wall VC setup

---
+++ Share on the Wall/ShareOnWallViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ShareOnWallViewController.swift
+//  VIT Share
+//
+//  Created by Likhit Garimella on 10/07/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class ShareOnWallViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
