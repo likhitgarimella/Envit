d4153b2e2ea1de8ede1000f6d676997848ce77b2
Likhit <likhitgmella@gmail.com>
2020-07-11 11:45:25 +0530

    PostApi #2

--- VIT Share/PostApi.swift
+++ VIT Share/PostApi.swift
@@ -7,7 +7,39 @@
 //
 
 import Foundation
+import Firebase
+
+/// Write your own Api, to conveniently observe database data...
 
 class PostApi {
     
-}   // #14
+    var REF_POSTS = Database.database().reference().child("Posts").child("Details")
+    
+    func observePosts(completion: @escaping (Post) -> Void) {
+        
+        REF_POSTS.observe(.childAdded, with: { (snapshot) in
+            
+            if let dict = snapshot.value as? [String: Any] {
+                let newPost = Post.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(newPost)
+            }
+            
+        })
+        
+    }
+    
+    func observePost(withId id: String, completion: @escaping (Post) -> Void) {
+        
+        REF_POSTS.child(id).observeSingleEvent(of: .value, with: {
+            snapshot in
+            
+            if let dict = snapshot.value as? [String:Any] {
+                let post = Post.transformPostPhoto(dict: dict, key: snapshot.key)
+                completion(post)
+            }
+            
+        })
+        
+    }
+    
+}   // #46
