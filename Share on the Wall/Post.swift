1d71aa908ba24013f40fbdf6cb8abb0faac5e7a1
Likhit <likhitgmella@gmail.com>
2020-07-11 11:41:03 +0530

    Post.swift #1

---
+++ Share on the Wall/Post.swift
@@ -0,0 +1,67 @@
+//
+//  Post.swift
+//  VIT Share
+//
+//  Created by Likhit Garimella on 11/07/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class Post {
+    
+    var caption: String?
+    var photoUrl: String?
+    
+    var uid: String?
+    
+    /// Remodel Post class, bcuz it currently doesn't have a post id property
+    var id: String?
+    
+    var likeCount: Int?
+    var likes: Dictionary<String, Any>?
+    
+    var isLiked: Bool?
+    
+}
+
+extension Post {
+    
+    // Photo
+    static func transformPostPhoto(dict: [String: Any], key: String) -> Post {
+        
+        let post = Post()
+        /// Remodel Post class, bcuz it currently doesn't have a post id property
+        post.id = key
+        post.caption = dict["caption"] as? String
+        post.photoUrl = dict["photoUrl"] as? String
+        post.uid = dict["uid"] as? String
+        
+        post.likeCount = dict["likeCount"] as? Int
+        post.likes = dict["likes"] as? Dictionary<String, Any>
+        
+        if let currentUserId = Auth.auth().currentUser?.uid {
+            if post.likes != nil {
+                /* if post.likes[currentUserId] != nil {
+                    post.isLiked = true
+                } else {
+                    post.isLiked = false
+                } */
+                /// Above commented snippet can be put in 1 line.. as below..
+                post.isLiked = post.likes![currentUserId] != nil
+            }
+        }
+        
+        return post
+        
+    }
+    
+    // Video
+    static func transformPostVideo() {
+        
+        
+        
+    }
+    
+}   // #68
