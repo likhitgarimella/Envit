0764c02505132eab1780fc8340c7969459eebe4b
Likhit <likhitgmella@gmail.com>
2020-07-12 11:42:18 +0530

    app user transformed

--- VIT Share/AppUser.swift
+++ VIT Share/AppUser.swift
@@ -15,19 +15,23 @@ class AppUser {
     var phoneString: String?
     var blockString: String?
     
+    var id: String?
+    
 }
 
 extension AppUser {
     
-    static func transformUser(dict: [String: Any]) -> AppUser {
+    static func transformUser(dict: [String: Any], key: String) -> AppUser {
         
         let user = AppUser()
         user.nameString = dict["1) Name"] as? String
         user.emailString = dict["2) Email"] as? String
         user.phoneString = dict["3) Phone"] as? String
         user.blockString = dict["4) Block"] as? String
+        
+        user.id = key
         return user
         
     }
     
-}   // #34
+}   // #38
