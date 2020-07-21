6f05a771b1cf846c300b2393b5ee894116aa65bc
Likhit <likhitgmella@gmail.com>
2020-07-21 22:42:59 +0530

    coll view files added

---
+++ Share on the Wall/CategoriesCollectionCell.swift
@@ -0,0 +1,18 @@
+//
+//  CategoriesCollectionCell.swift
+//  VIT Share
+//
+//  Created by Likhit Garimella on 21/07/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesCollectionCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}   // #19
