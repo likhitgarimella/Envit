7e4ba098c68601d267e6501755d5e35ec7dd8524
Likhit <likhitgmella@gmail.com>
2020-07-21 18:50:18 +0530

    category scroll view - shit #1

--- Share on the Wall/ShareOnWallViewController.swift
+++ Share on the Wall/ShareOnWallViewController.swift
@@ -53,8 +53,36 @@ class ShareOnWallViewController: UIViewController {
         
     }
     
+    var scView: UIScrollView!
+    let buttonPadding: CGFloat = 10
+    var xOffset: CGFloat = 10
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scView = UIScrollView(frame: CGRect(x: 0, y: 600, width: view.bounds.width, height: 50))
+        view.addSubview(scView)
+        
+        scView.backgroundColor = UIColor.white
+        scView.translatesAutoresizingMaskIntoConstraints = false
+        
+        for i in 0 ... 10 {
+            
+            let button = UIButton()
+            button.tag = i
+            button.backgroundColor = UIColor(red: 255/255, green: 231/255, blue: 231/255, alpha: 1.0)
+            button.layer.cornerRadius = 15
+            button.setTitle("\(i)", for: .normal)
+            //button.addTarget(self, action: #selector(btnTouch), for: UIControlEvents.touchUpInside)
+
+            button.frame = CGRect(x: xOffset, y: CGFloat(buttonPadding), width: 80, height: 30)
+
+            xOffset = xOffset + CGFloat(buttonPadding) + button.frame.size.width
+            scView.addSubview(button)
+            
+        }
+
+        scView.contentSize = CGSize(width: xOffset, height: scView.frame.height)
         
         hideKeyboardWhenTappedAround()
         
@@ -212,4 +240,4 @@ extension ShareOnWallViewController: UIImagePickerControllerDelegate, UINavigati
         dismiss(animated: true, completion: nil)
     }
     
-}   // #216
+}   // #244
