27ec66cad6d1b93fa52fcf47e3fd3cd29713c771
Likhit <likhitgmella@gmail.com>
2020-07-26 19:27:04 +0530

    profile screen logout functionality added

--- Profile/ProfileViewController.swift
+++ Profile/ProfileViewController.swift
@@ -114,4 +114,20 @@ class ProfileViewController: UIViewController {
         
     }
     
-}   // #118
+    /// Logout functionality
+    @IBAction func logoutAction(_ sender: UIButton) {
+        
+        /// using AuthService class
+        AuthService.logout(onSuccess: {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let signInVC = storyboard.instantiateViewController(withIdentifier: "LoginViewController")
+            self.present(signInVC, animated: true, completion: nil)
+        }, onError: {
+            (errorMessage) in
+            /// implement hud here
+            print(errorMessage)
+        })
+        
+    }
+    
+}   // #134
