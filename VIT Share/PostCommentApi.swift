502560049a42206ec943e7f5013bcf01d2eac719
Likhit <likhitgmella@gmail.com>
2020-07-28 02:20:44 +0530

    post comment api done

--- VIT Share/PostCommentApi.swift
+++ VIT Share/PostCommentApi.swift
@@ -7,7 +7,18 @@
 //
 
 import Foundation
+import Firebase
+
+/// Write your own Api, to conveniently observe database data...
 
 class PostCommentApi {
     
-}   // #14
+    var REF_POST_COMMENTS = Database.database().reference().child("Wall-Post-Comments")
+    
+    func observePostComments(withPostId id: String) {
+        
+        
+        
+    }
+    
+}   // #25
