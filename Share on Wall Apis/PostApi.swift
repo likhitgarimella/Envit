bdf1041c03f75a7f553741bcd80173ab0b2265fa
Likhit <likhitgmella@gmail.com>
2020-08-25 02:50:21 +0530

    other like params added in post api

--- Share on Wall Apis/PostApi.swift
+++ Share on Wall Apis/PostApi.swift
@@ -42,4 +42,54 @@ class PostApi {
         
     }
     
-}   // #46
+    func observeLikeCount(withPostId id: String, completion: @escaping (Int) -> Void) {
+        
+        REF_POSTS.child(id).observe(.childChanged, with: {
+            snapshot in
+            print(snapshot)
+            if let value = snapshot.value as? Int {
+                completion(value)
+            }
+        })
+        
+    }
+    
+    func incrementLikes(postId: String, onSuccess: @escaping (Post) -> Void, onError: @escaping (_ errorMessage: String?) -> Void ) {
+        
+        let postRef = Api.Post.REF_POSTS.child(postId)
+        postRef.runTransactionBlock ({ (currentData: MutableData) -> TransactionResult in
+            if var post = currentData.value as? [String: AnyObject], let uid = Api.UserDet.CURRENT_USER?.uid {
+                // print("Value 1: \(currentData.value)")
+                var likes: Dictionary<String, Bool>
+                likes = post["likes"] as? [String: Bool] ?? [:]
+                var likeCount = post["likeCount"] as? Int ?? 0
+                if let _ = likes[uid] {
+                    likeCount -= 1
+                    likes.removeValue(forKey: uid)
+                } else {
+                    likeCount += 1
+                    likes[uid] = true
+                }
+                post["likeCount"] = likeCount as AnyObject
+                post["likes"] = likes as AnyObject
+                
+                currentData.value = post
+                
+                return TransactionResult.success(withValue: currentData)
+            }
+            return TransactionResult.success(withValue: currentData)
+        }) { (error, committed, snapshot) in
+            if let error = error {
+                onError(error.localizedDescription)
+            }
+            // print("Value 2: \(snapshot?.value)")
+            if let dict = snapshot?.value as? [String: Any] {
+                let post = Post.transformPostPhoto(dict: dict, key: snapshot!.key)
+                onSuccess(post)
+            }
+            
+        }
+        
+    }
+    
+}   // #96
