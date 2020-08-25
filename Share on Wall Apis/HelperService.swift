bb1fb95065b3533bf864b3c110ba2a836dd6e04c
Likhit <likhitgmella@gmail.com>
2020-08-25 13:52:12 +0530

    helper service backend done

--- Share on Wall Apis/HelperService.swift
+++ Share on Wall Apis/HelperService.swift
@@ -7,7 +7,59 @@
 //
 
 import Foundation
+import FirebaseStorage
 
 class HelperService {
     
-}   // #14
+    static func uploadDataToServer(data: Data, caption: String, frame: String, category: String, onSuccess: @escaping () -> Void) {
+        // NSUUID
+        let photoIdString = NSUUID().uuidString
+        print("Photo Id String: \(photoIdString)")
+        let storageRef = Storage.storage().reference(forURL: "gs://vit-share.appspot.com").child("Posts").child(photoIdString)
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpg"
+        // put image data
+        storageRef.putData(data, metadata: nil) { (metadata, error) in
+            if error != nil {
+                print(error!.localizedDescription)
+                return
+            }
+            // get download url for image from Firebase Storage
+            storageRef.downloadURL { (url, error) in
+                // convert that download url to string
+                if let metaImageUrl = url?.absoluteString {
+                    print(metaImageUrl)
+                    self.sendDataToDatabase(photoUrl: metaImageUrl, caption: caption, frame: frame, category: category, onSuccess: onSuccess)
+                }
+            }
+        }
+    }
+    
+    static func sendDataToDatabase(photoUrl: String, caption: String, frame: String, category: String, onSuccess: @escaping () -> Void) {
+        let newPostId = Api.Post.REF_POSTS.childByAutoId().key
+        let newPostReference = Api.Post.REF_POSTS.child(newPostId!)
+        guard let currentUser = Api.UserDet.CURRENT_USER else {
+            return
+        }
+        let currentUserId = currentUser.uid
+        // put that download url string in db
+        newPostReference.setValue(["uid": currentUserId, "photoUrl": photoUrl, "caption": caption, "frameColor": frame, "categoryLabel": category], withCompletionBlock: { (error, ref) in
+            if error != nil {
+                print(error!.localizedDescription)
+                return
+            }
+            
+            // reference for my posts
+            let myPostRef = Api.MyPosts.REF_MYPOSTS.child(currentUserId).child(newPostId!)
+            myPostRef.setValue(true, withCompletionBlock: {
+                (error, ref) in
+                if error != nil {
+                    print(error!.localizedDescription)
+                    return
+                }
+            })
+            onSuccess()
+        })
+    }
+    
+}   // #66
