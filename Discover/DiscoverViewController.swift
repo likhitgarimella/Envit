3bf19f24c9f873395520158fefd1f72d3fa87d59
Likhit Garimella <likhitgmella@gmail.com>
2020-09-23 09:59:55 +0530

    hierarchy change; new discover VC

---
+++ Discover/DiscoverViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DiscoverViewController.swift
+//  VIT Share
+//
+//  Created by Likhit Garimella on 23/09/20.
+//  Copyright © 2020 Likhit Garimella. All rights reserved.
+//
+
+import UIKit
+
+class DiscoverViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}   // #21
